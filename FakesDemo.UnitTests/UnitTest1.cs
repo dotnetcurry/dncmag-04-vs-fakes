ee14aa6c3c189a7b4c24dc65744f8fd6c3cc6b83
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2012-12-10 17:52:23 +0530

    Code Commit

---
+++ FakesDemo.UnitTests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Fakes;
+using System.IO.Fakes;
+using FakesDemo.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.QualityTools.Testing.Fakes.Stubs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakesDemo;
+
+namespace FakesDemo.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimDateTime.NowGet = () => new DateTime(2012, 12, 21);
+                ClassB.DoomsDay();
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (ShimsContext.Create())
+            {
+                ShimDirectory.BehaveAsNotImplemented();
+                ClassB.DeleteC();
+            }
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            var stubLogger = new StubILogger {ShouldLog = () => true};
+            var sut = new ClassA();
+            var observer = new StubObserver();
+            stubLogger.InstanceObserver = observer;
+            
+            //Act
+            sut.GetName(stubLogger);
+            
+            var calls = observer.GetCalls();
+
+        }
+    }
+}
---
+++ FakesDemo/ClassA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakesDemo
+{
+    public interface ILogger{
+        bool ShouldLog();
+        void Log(string message);
+    }
+
+    public class ClassA
+    {
+        public string GetName(ILogger logger)
+        {
+            if (logger.ShouldLog()){
+                logger.Log("log something");
+                return "some name";
+            }
+
+            return string.Empty;
+        }
+    }
+
+
+    public class ClassB
+    {
+        public static void DoomsDay()
+        {
+            if (DateTime.Now == new DateTime(2012, 12, 21))
+                throw new Exception("Boom!");
+        }
+
+
+        //Do not run this in the real code!
+        //public static void DeleteC()
+        //{
+        //    Directory.Delete(@"c:\", true);
+        //}
+
+    }
+}
